d5ce8e29de9cc56b1ac2c57228a531fac7819715
Felipe Amador <felipedavidamador@gmail.com>
2021-10-19 21:08:56 -0300

    feat: add script to run the server

---
+++ server.py
@@ -0,0 +1,17 @@
+import uvicorn
+
+from fastapi import FastAPI
+
+def create_app() -> FastAPI:
+    return FastAPI()
+
+app = create_app()
+
+
+@app.get('/')
+def ping():
+    return { "message": "pong" }
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
